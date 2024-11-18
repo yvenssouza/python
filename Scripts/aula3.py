55ec89871a5e8800ebdb00e1c18b020613d87980
yvens <yvenssouza16@gmail.com>
2024-11-17 22:54:03 -0400

    aulas

---
+++ Scripts/aula3.py
@@ -0,0 +1,23 @@
+"""
+Python = Linguagem de programação 
+Tipo de tipagem = dinâmica / Forte
+str -> string ->texto
+Strings são textos que estão dentro de aspas
+"""
+
+print(1234)
+
+#Aspas simples
+print('Yvens Souza')
+print(1,'Yvens "Souza"')
+
+
+#Aspas duplas 
+print("Yvens Souza")
+print("Yvens 'Souza'")
+
+#Escape
+print("Yvens \"Souza\"")
+
+# r
+print(r"Yvens \"Souza\"")
